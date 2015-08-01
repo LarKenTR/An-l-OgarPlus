ab3f3203d23296cac91ea3b158e239fd604c9dad
DaFudgeWizzad <seth5728@gmail.com>
2015-08-01 04:44:31 -0400

    Added custom server startup message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var GameServer = require('./GameServer');
 var showConsole = true;
 
 // Start msg
-console.log("[Game] Ogar - An open source Agar.io server implementation");
+console.log("[Seth] HI!");
 
 // Handle arguments
 process.argv.forEach(function(val) {
